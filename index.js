13d8ae6646ebaf0980f3c8cd25008f105eb764e2
Daniel Park <parkdk96@gmail.com>
2019-08-16 03:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,9 @@ function logWhisper(string) {
 function sayHighToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
+  } else if (string.toUpperCase() === string) {
+    return "YES INDEED!"
+  } else {
+    return "I love you, too."
   }
 }
